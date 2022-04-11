47245091e8c31f5c60ba09105ab15ec8703a2bf4
aymen2709 <aymen.abid4070@gmail.com>
2022-04-11 13:46:12 +0100

    Map working

--- src/MapPage.js
+++ src/MapPage.js
@@ -1,76 +1,75 @@
-import {  View,Text,  StyleSheet  } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 import MapView, { Marker, PROVIDER_DEFAULT } from 'react-native-maps';
 import { Constants, Location, Permissions } from 'expo';
 
- 
+
 
 
 
 
 
 const MapPage = () => {
-    ComponentDidMount=()=> {
+    ComponentDidMount = () => {
         this._getLocationAsync();
+    }
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                locationResult: 'Permission to access location was denied',
+                location,
+            });
         }
-      
-       
-      
-       _getLocationAsync = async () => {
-         let { status } = await Permissions.askAsync(Permissions.LOCATION);
-         if (status !== 'granted') {
-           this.setState({
-             locationResult: 'Permission to access location was denied',
-             location,
-           });
-         }
-      
-         let location = await Location.getCurrentPositionAsync({});
-      console.Log("MYLIVELOCATION",""+JSON.stringify(location));
-       };
-    
+
+        let location = await Location.getCurrentPositionAsync({});
+        console.Log("MYLIVELOCATION", "" + JSON.stringify(location));
+        
+    };
+
     return (
         <View style={styles.container}>
-            <MapView 
-            style={styles.map}
-            
+            <MapView
+                style={styles.map}
+
                 initialRegion={{
                     latitude: 35.820918,
-                    longitude:  10.592252,
+                    longitude: 10.592252,
                     latitudeDelta: 0.015,
                     longitudeDelta: 0.0121,
                 }}
-                
-                
+
+
                 provider={PROVIDER_DEFAULT}>
                 <MapView.UrlTile
-                  urlTemplate={"http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"}
-                  shouldReplaceMapContent={true}>
-            </MapView.UrlTile>
-                
-               <Marker
-                coordinate={{
-                    latitude: 35.820918,
-                longitude: 10.592252,     
-                }}    
-            ></Marker>
-            
+                    urlTemplate={"http://a.tile.openstreetmap.org/{z}/{x}/{y}.png"}
+                    shouldReplaceMapContent={true}>
+                </MapView.UrlTile>
 
-              
-             
+                <Marker
+                    coordinate={{
+                        latitude: 35.820918,
+                        longitude: 10.592252,
+                    }}
+                ></Marker>
 
-             </MapView>
-            
-             
-            
 
-                
+
+
+
+            </MapView>
+
+
+
+
+
         </View>
-          
+
     )
 }
 
-    
+
 
 
 
@@ -81,17 +80,17 @@ MapPage.navigationOptions = {
 
 const styles = StyleSheet.create({
     container: {
-        
+
         flex: 1,
         backgroundColor: '#ffffff00',
         alignItems: 'center',
         justifyContent: 'center',
     },
-    map:{
-        
+    map: {
+
         flex: 1,
-        width:"100%",
-        
+        width: "100%",
+
         alignItems: 'center',
         justifyContent: 'center',
 
