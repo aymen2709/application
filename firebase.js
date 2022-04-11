bf4e09f004125755275cbfb5a2e1192f17195d2e
anasbenfarah <anasbenfarah1997@gmail.com>
2022-04-11 10:45:24 +0000

    first commit

---
+++ firebase.js
@@ -0,0 +1,15 @@
+
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCZmc_bWSzHPClO0FTIbGeALasuGJdP7Uk",
+  authDomain: "houmti-ndhifa.firebaseapp.com",
+  projectId: "houmti-ndhifa",
+  storageBucket: "houmti-ndhifa.appspot.com",
+  messagingSenderId: "421661986639",
+  appId: "1:421661986639:web:aab3cbb3661937ac1d3f3a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
